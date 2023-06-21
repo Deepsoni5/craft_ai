a614cc69703d92cd062c478a166cb7dffb04335d
Deep  soni <fichadiyadeep@gmail.com>
2023-06-21 17:10:15 +0000

    home page

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,10 +1,14 @@
+"use client"
+import Hero from '@/components/layouts/Hero'
 import NavBar from '@/components/layouts/Navbar'
 import Image from 'next/image'
+
 
 export default function Home() {
   return (
     <>
     <NavBar/>
+    <Hero/>
     
     </>
      )
