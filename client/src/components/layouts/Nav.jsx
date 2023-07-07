6de29f3f5041047bfd353c9beeb634d92fc45908
Deepsoni5 <104735566+Deepsoni5@users.noreply.github.com>
2023-07-07 15:13:45 +0000

    color nav fix

--- client/src/components/layouts/Nav.jsx
+++ client/src/components/layouts/Nav.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 
-export default function Nav({user}) {
+export default function Nav({ user }) {
 
-   
+
     const [open, setOpen] = useState(false)
     const logout = () => {
-        window.open("http://localhost:8000/auth/logout","_self")
+        window.open("http://localhost:8000/auth/logout", "_self")
     }
-   
+
     return (
 
-        <div className="overflow-x-hidden bg-gray-50">
+        <div className="overflow-x-hidden bg-gray-800">
             <header className="py-4 md:py-6" x-data="{expanded: false}">
                 <div className="container px-4 mx-auto sm:px-6 lg:px-8">
                     <div className="flex items-center justify-between">
@@ -42,52 +42,56 @@ export default function Nav({user}) {
                         </div>
 
                         <div className="hidden lg:flex lg:ml-16 lg:items-center lg:justify-center lg:space-x-10 xl:space-x-16">
-                            <a href="#" title="" className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> Features </a>
+                            <a href="#" title="" className="text-base font-medium p-2  transition-all duration-200 rounded focus:outline-none font-pj text-gray-400 hover:bg-gray-700 hover:rounded hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"> Features </a>
 
-                            <a href="#" title="" className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> Pricing </a>
+                            <a href="#" title="" className="text-base font-medium p-2  transition-all duration-200 rounded focus:outline-none font-pj text-gray-400 hover:bg-gray-700 hover:rounded hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"> Pricing </a>
 
-                            <a href="#" title="" className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"> Automation </a>
+                            <a href="#" title="" className="text-base font-medium p-2  transition-all duration-200 rounded focus:outline-none font-pj text-gray-400 hover:bg-gray-700 hover:rounded hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"> Automation </a>
                         </div>
-                  
+
                         <div className="hidden lg:ml-auto lg:flex lg:items-center lg:space-x-10">
 
                             {
-                                !user ? 
+                                !user ?
                                     <div>
-                            
-                            <a
-                                href="/login"
-                                title=""
-                                className="inline-flex items-center justify-center px-6 py-3 text-base font-bold leading-7 text-white transition-all duration-200 bg-gray-900 border border-transparent rounded-xl hover:bg-gray-600 font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
-                                role="button"
-                            >
-                                Login
+
+                                        <a
+                                            href="/login"
+                                            title=""
+                                            className="inline-flex items-center justify-center px-6 py-3 text-base font-bold leading-7 text-white transition-all duration-200 bg-gray-900 border border-transparent rounded-xl hover:bg-gray-600 font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                                            role="button"
+                                        >
+                                            Login
                                         </a>
-                                        </div>
+                                    </div>
                                     :
                                     <div className='flex'>
-                             <div
-                             onClick={logout}
-                                href="#"
-                                title=""
-                                className="inline-flex items-center justify-center px-6 py-3 text-base font-bold leading-7 text-white transition-all duration-200 bg-gray-900 border border-transparent rounded-xl hover:bg-gray-600 font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
-                                role="button"
-                            >
-                                Logout
-                                        </div>
+                                          <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                                            <span class="relative px-6 py-3 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                               Try Now
+                                            </span>
+                                        </button>
+                                        <button onClick={logout} class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                                            <span class="relative px-6 py-3 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                               Logout
+                                            </span>
+                                        </button>
+                                       
                                         {
-                                            user.provider=="discord" ? 
+                                            user.provider == "discord" ?
                                                 <img className='w-[50px] h-[50px] ml-4 rounded-full' src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}`} />
                                                 :
                                                 <img className='w-[50px] h-[50px] ml-4 rounded-full' src={user.photos[0].value} />
 
                                         }
-                                        </div>
-                                
+
+                                    </div>
+
+
                             }
-                            </div>
-                         
-                
+                        </div>
+
+
                     </div>
 
                     {open ?
